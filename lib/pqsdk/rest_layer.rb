89dc25844c9eb0b9dab261be720902a704df306a
Francesco Boffa <fra.boffa@gmail.com>
2016-01-27 16:02:35 +0100

    Refactored RestLayer

--- lib/pqsdk/rest_layer.rb
+++ lib/pqsdk/rest_layer.rb
@@ -1,61 +1,54 @@
 module PQSDK
+  # A small wrapper to the Faraday gem to make get/post/put requests to the API
+  # server.
   class RestLayer
     def self.get(endpoint, parameters = {}, headers = {})
-      conn = Faraday.new(Settings.api_root)
+      res = connection.get endpoint, parameters, headers
 
-      res = conn.get endpoint, parameters, headers
-
-      check_status(res.status.to_i, res.body)
+      check_result(res)
 
       begin
-        [ res.status.to_i, JSON.parse(res.body), res.headers ]
+        [res.status.to_i, JSON.parse(res.body), res.headers]
       rescue JSON::ParserError
-        [ res.status.to_i, nil, res.headers ]
+        [res.status.to_i, nil, res.headers]
       end
     end
 
     def self.post(endpoint, parameters = {}, headers = {})
-      conn = Faraday.new(Settings.api_root)
+      parameters = parameters.to_json if headers['Content-Type'] == 'application/json'
+      res = connection.post endpoint, parameters, headers
 
-      if headers['Content-Type'] == 'application/json'
-        res = conn.post endpoint, parameters.to_json, headers
-      else
-        res = conn.post endpoint, parameters, headers
-      end
-
-      check_status(res.status.to_i, res.body)
+      check_result(res)
 
       begin
-        [ res.status.to_i, JSON.parse(res.body), res.headers ]
+        [res.status.to_i, JSON.parse(res.body), res.headers]
       rescue JSON::ParserError
-        [ res.status.to_i, nil, res.headers ]
+        [res.status.to_i, nil, res.headers]
       end
     end
 
     def self.put(endpoint, parameters = {}, headers = {})
-      conn = Faraday.new(Settings.api_root)
+      parameters = parameters.to_json if headers['Content-Type'] == 'application/json'
+      res = connection.put endpoint, parameters, headers
 
-      if headers['Content-Type'] == 'application/json'
-        res = conn.put endpoint, parameters.to_json, headers
-      else
-        res = conn.put endpoint, parameters, headers
-      end
-
-      check_status(res.status.to_i, res.body)
+      check_result(res)
 
       begin
-        [ res.status.to_i, JSON.parse(res.body), res.headers ]
+        [res.status.to_i, JSON.parse(res.body), res.headers]
       rescue JSON::ParserError
-        [ res.status.to_i, nil, res.headers ]
+        [res.status.to_i, nil, res.headers]
       end
     end
 
-  private
-    def self.check_status(code, body)
-      if code >= 500
-        raise Exception.new("Internal Server Error: " + body)
-      elsif code == 401
-        raise Exception.new("You are not authorized to perform that request")
+    def self.connection
+      Faraday.new(Settings.api_root)
+    end
+
+    def self.check_result(result)
+      if result.status.to_i >= 500
+        fail "Internal Server Error: #{result.body}"
+      elsif result.status.to_i == 401
+        fail 'You are not authorized to perform that request'
       end
     end
   end
