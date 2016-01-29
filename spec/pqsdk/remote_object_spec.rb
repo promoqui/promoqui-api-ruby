ae0ad2d7ebc69300fcff852327c843e8f0245e10
Francesco Boffa <fra.boffa@gmail.com>
2016-01-29 09:33:39 +0100

    RemoteObject#save specs

--- spec/pqsdk/remote_object_spec.rb
+++ spec/pqsdk/remote_object_spec.rb
@@ -61,6 +61,59 @@ describe PQSDK::RemoteObject do
     end
   end
 
+  describe '#save' do
+    before { allow(PQSDK::Token).to receive(:access_token).and_return 123 }
+
+    it 'returns false when the object is not valid' do
+      s = FakeObject.new(name: 'Fake')
+      allow(s).to receive(:valid?).and_return false
+
+      expect(s.save).to be_falsey
+    end
+
+    it 'returns true if the object gets saved' do
+      allow(PQSDK::RestLayer).to receive(:post)
+        .and_return([201, {'id' => 1}, {}])
+
+      s = FakeObject.new(name: 'Fake')
+      expect(s.save).to be_truthy
+    end
+
+    context 'when the object is persisted' do
+      let(:o) { FakeObject.new(id: 1, name: 'Fake') }
+
+      it 'sends a put request to the object' do
+        expect(PQSDK::RestLayer).to receive(:put)
+          .with(%r(v\d+/fakes/1$), {'name' => 'Fake'}, 'Authorization' => 'Bearer 123')
+          .and_return([200, {}, {}])
+        o.save
+      end
+
+      it 'raises an error if the response is not 200' do
+        allow(PQSDK::RestLayer).to receive(:put)
+          .and_return([400, {}, {}])
+        expect { o.save }.to raise_error(RuntimeError)
+      end
+    end
+
+    context 'when the object is not persisted' do
+      let(:o) { FakeObject.new(name: 'Fake') }
+
+      it 'sends a post request to the collection' do
+        expect(PQSDK::RestLayer).to receive(:post)
+          .with(%r(v\d+/fakes$), {'name' => 'Fake'}, 'Authorization' => 'Bearer 123')
+          .and_return([201, {}, {}])
+        o.save
+      end
+
+      it 'raises an error if the response is not 201 or 203' do
+        allow(PQSDK::RestLayer).to receive(:post)
+          .and_return([400, {}, {}])
+        expect { o.save }.to raise_error(RuntimeError)
+      end
+    end
+  end
+
   describe '#to_json' do
     it 'returns a JSON serialization of the object' do
       s = FakeObject.new(name: 'Fake')
